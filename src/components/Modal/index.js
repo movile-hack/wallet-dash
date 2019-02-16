0ef3d0f60024a204be9abf6c6b65fb0b5e22414f
Alexandre dos Santos Soares <ale_santos.soares@hotmail.com>
2019-02-16 20:35:40 -0200

    component: created modal custom

---
+++ src/components/Modal/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { Modal } from 'antd'
+import PropTypes from 'prop-types'
+import './index.css'
+
+import warning from '../../assets/icons/warning.svg'
+
+class ModalCustom extends Component {
+  render() {
+    const {
+      title,
+      content,
+      visible,
+      handleConfirmation,
+    } = this.props 
+    return (
+      <Modal
+        visible={visible}
+        onOk={() => handleConfirmation('sale')}
+        onCancel={handleConfirmation}
+      >
+        <div className="modalContentWrapper">
+          <img className="iconImage" src={warning} alt="alert" title="warning" />
+          <h1>{title}</h1>
+          <p>{content}</p>
+        </div>
+      </Modal>
+    )
+  }
+}
+
+ModalCustom.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  visible: PropTypes.bool.isRequired,
+  handleConfirmation: PropTypes.func.isRequired,
+}
+
+export default ModalCustom
