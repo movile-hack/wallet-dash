355e68615f0284f90d580c89cf670be2860f8962
Caio Flores <caiopflores@gmail.com>
2019-02-17 00:32:27 -0300

    feat: add loading

---
+++ src/components/Loader/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Icon } from 'antd'
+
+const Loading = () => {
+  return <Icon type="loading" style={{ fontSize: '5em', marginLeft: '50%', marginTop: '10%' }} />
+}
+
+export default Loading
--- src/pages/Product/List/index.js
+++ src/pages/Product/List/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import ListProductContainer from '../../../containers/Product/List'
 import ProductService from '../../../service/product'
+import Loading from '../../../components/Loader'
 
 class List extends Component {
   productService = null
   state = {
-    products: []
+    products: false
   }
 
   componentDidMount() {
@@ -14,22 +15,17 @@ class List extends Component {
   }
 
   async handleProductList() {
-   try {
-    // const { data: products } = await this.productService.getAllProducts()
-    const products =  this.productService.getAllProducts()
-    this.setState({ products })
-   } catch (error) {
-     console.log(error)
-   }
+    try {
+      const products = await this.productService.getAllProducts()
+      this.setState({ products })
+    } catch (error) {
+      console.log(error)
+    }
   }
-  render() { 
-    const { products } = this.state;
-    return (
-      <ListProductContainer
-        products={products}
-      />  
-    )
+  render() {
+    const { products } = this.state
+    return products ? <ListProductContainer products={products} /> : <Loading />
   }
 }
- 
+
 export default List
