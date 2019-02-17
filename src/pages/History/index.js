2d7407395de68eb90932e3258554251b24ffb2e7
Alexandre dos Santos Soares <ale_santos.soares@hotmail.com>
2019-02-17 04:47:21 -0300

    pages: use service transactions

--- src/pages/History/index.js
+++ src/pages/History/index.js
@@ -1,10 +1,55 @@
 import React, { Component } from 'react'
 import SalesHistoryContainer from '../../containers/SalesHistory'
+import TransactionSellService from '../../service/transactionsSell'
 
 class SalesHistory extends Component {
- 
-  render() { 
-    return (<SalesHistoryContainer />)
+  transactionSellService = null
+  state = {
+    transactions: [],
+  }
+
+  componentDidMount() {
+    this.transactionSellService = new TransactionSellService()
+    this.handleGetAllTransaction()
+  }
+
+  parseItemsTransaction = ({ items }) => {
+    let array = []
+    for(let key in items) {
+      array = [...array, ...items[key].items]
+    }
+
+    const transactions = array.map(item => ({
+        id: item.id,
+        description: item.description,
+        date: item.date,
+        value: item.amount.split(''),
+        image: 'notes',
+        type: item.type,
+      })
+    ).filter(
+      item => item.type === "transfer"
+    )
+
+    return this.setState({ transactions })
+  }
+
+  handleGetAllTransaction = async () => {
+    try {
+      const transactions = await this.transactionSellService.getAll()
+      this.parseItemsTransaction(transactions)
+    } catch (error) {
+      
+    }
+  }
+
+  render() {
+    const { transactions } = this.state
+    return (
+      <SalesHistoryContainer 
+        transactions={transactions}
+      />
+    )
   }
 }
  
